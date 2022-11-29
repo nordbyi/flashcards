05fe8242a92591988b623a6c47100296c58fd9c0
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-29 16:43:30 -0700

    Add initial returnCurrentCard() method test

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,20 +1,24 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const Round = require('../src/Round')
-const Deck = require('../src/Deck')
-const Card = require('../src/Card') // need this?
-const data = require('../src/data')
+const Round = require("../src/Round");
+const Deck = require("../src/Deck");
+const Card = require("../src/Card"); // need this?
+const data = require("../src/data");
 
-describe('Round', function () {
-  let round
-  let deck
+describe("Round", function () {
+  let round;
+  let deck;
 
-  beforeEach( function () {
-    round = new Round()
-    deck = new Deck(data.slice(0, 3).map(el => el.id, el.question, el.answers, el.correctAnswer))
-  })
-  
+  beforeEach(function () {
+    deck = new Deck(
+      data.prototypeData.slice(0, 3).map(
+        (el) => new Card(el.id, el.question, el.answers, el.correctAnswer)
+      )
+    );
+    round = new Round(deck);
+  });
+
   it("Should be a function", () => {
     expect(Round).to.be.a("function");
   });
@@ -23,7 +27,11 @@ describe('Round', function () {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('Should have a current Card', () => {
-    expect(round.currentCard).to.deep.equal(deck.cards[0])
+  it("Should have a current Card", () => {
+    expect(round.currentCard).to.deep.equal(deck.cards[0]);
+  });
+
+  it('Should be able to return the currentCard', () => {
+    expect(round.returnCurrentCard().to.deep(deck.cards[0]))
   })
-})
+});
