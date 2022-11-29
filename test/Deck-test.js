fce5745cf671454a7d6272ed73208470350216ea
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-29 15:04:38 -0700

    Add initial tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,1 +1,20 @@
+const chai = require("chai");
+const expect = chai.expect;
 
+const Deck = require("../src/Deck");
+
+describe('Deck', function () {
+  let deck
+
+  beforeEach(function () {
+    deck = new Deck()
+  })
+
+  it('Should be a function', () => {
+    expect(Deck).to.be.a("function");
+  })
+
+  it('Should be an instance of Deck', () => {
+    expect(deck).to.be.an.instanceof(Deck);
+  })
+})
