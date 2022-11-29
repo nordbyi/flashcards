fa979f031c721dd7975955d516b34aed2c48574f
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-29 14:10:09 -0700

    Refactor sad path tests into own tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -42,16 +42,28 @@ describe("Turn", function () {
   });
 
   it("Should be able to return the card", () => {
-    expect(turn.returnCard()).to.equal(turn.card);
+    expect(turn.returnCard()).to.equal(card);
   });
 
   it("Should be able to indicate if a userGuess equals the correct answer in the card", () => {
     expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it("Should be able to indicate if a userGuess does not equal the correct answer in the card", () => {
     expect(turn2.evaluateGuess()).to.equal(false);
   });
 
   it('Should be able to tell you if the guess is correct', () => {
     expect(turn.giveFeedback()).to.equal('correct!')
+  })
+
+  it('Should be able to tell you if the guess is not correct', () => {
     expect(turn2.giveFeedback()).to.equal('incorrect!')
   })
 });
+
+// indicate if a user guess does not equal
+// tell you if guess is incorrect
+
+
+// push to github! Delete this beforehand!
