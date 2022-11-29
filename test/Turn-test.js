2d4c3cac2241e7770e1f80c9f11af36af6c98a6e
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-28 18:16:29 -0700

    Add test for returnGuess() method

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,30 +2,44 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const Turn = require("../src/Turn");
-const Card = require("../src/Card")
+const Card = require("../src/Card");
 
-describe('Turn', function () {
-  let turn
-  let card
+describe("Turn", function () {
+  let turn;
+  let turn2;
+  let card;
 
   beforeEach(function () {
-    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
-    turn = new Turn('sea otter', card)
-  })
+    card = new Card(
+      1,
+      "What is Robbie's favorite animal",
+      ["sea otter", "pug", "capybara"],
+      "sea otter"
+    );
+    turn = new Turn("sea otter", card);
 
-  it('Should be a function', () => {
-    expect(Turn).to.be.a('function')
-  })
+    turn2 = new Turn("pug", card);
+  });
 
-  it('Should be an instance of Turn', () => {
-    expect(turn).to.be.an.instanceof(Turn)
-  })
+  it("Should be a function", () => {
+    expect(Turn).to.be.a("function");
+  });
 
-  it('Should accept a guess as an argument', () => {
-    expect(turn.userGuess).to.equal('sea otter')
-  })
+  it("Should be an instance of Turn", () => {
+    expect(turn).to.be.an.instanceof(Turn);
+  });
 
-  it('Should accept an instance of Card as an argument as well', () => {
-    expect(turn.card).to.be.an.instanceof(Card)
-  })
-})
+  it("Should accept a guess as an argument", () => {
+    expect(turn.userGuess).to.equal("sea otter");
+  });
+
+  it("Should accept an instance of Card as an argument as well", () => {
+    expect(turn.card).to.be.an.instanceof(Card);
+  });
+
+  it("Should be able to return the userGuess", () => {
+    expect(turn.returnGuess()).to.equal(turn.userGuess);
+  });
+
+
+});
