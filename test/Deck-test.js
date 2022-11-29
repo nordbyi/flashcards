1fae85ea3892f314fc2fd257f0ea730aa8687bd2
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-29 15:06:17 -0700

    Add test temporary data

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,6 +3,23 @@ const expect = chai.expect;
 
 const Deck = require("../src/Deck");
 
+const testData = [{
+  "id": 1,
+  "question": "What allows you to define a set of related information using key-value pairs?",
+  "answers": ["object", "array", "function"],
+  "correctAnswer": "object"
+}, {
+  "id": 2,
+  "question": "What is a comma-separated list of related values?",
+  "answers": ["array", "object", "function"],
+  "correctAnswer": "array"
+}, {
+  "id": 3,
+  "question": "What type of prototype method directly modifies the existing array?",
+  "answers": ["mutator method", "accessor method", "iteration method"],
+  "correctAnswer": "mutator method"
+}]
+
 describe('Deck', function () {
   let deck
 
