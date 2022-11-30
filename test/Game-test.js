23bb7918220c1475fcc108f1c35485743207e997
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-30 16:30:33 -0700

    Fix bug in createCards() method test

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const Round = require("../src/Round");
 const Deck = require("../src/Deck");
-const Turn = require("../src/Turn")
+const Turn = require("../src/Turn");
 const Card = require("../src/Card");
 const data = require("../src/data");
 const Game = require("../src/Game");
@@ -12,28 +12,25 @@ describe("Game", function () {
   let round;
   let round2;
   let deck;
-  let turn
-  let game
+  let turn;
+  let game;
 
   beforeEach(function () {
     deck = new Deck(
-      data.prototypeData.slice(0, 3).map(
-        (el) => new Card(el.id, el.question, el.answers, el.correctAnswer)
-      )
+      data.prototypeData
+        .slice(0, 3)
+        .map((el) => new Card(el.id, el.question, el.answers, el.correctAnswer))
     );
     round = new Round(deck);
-    round2 = new Round(deck)
-    game = new Game()
+    round2 = new Round(deck);
+    game = new Game();
   });
 
-  it('Should have a currentRound property', () => {
-    expect(game.currentRound).to.be.undefined
-  })
+  it("Should have a currentRound property", () => {
+    expect(game.currentRound).to.be.undefined;
+  });
 
-  it('Should be able to create Cards', () => {
-    const cards = game.createCards()
-    expect(cards[0]).to.deep.equal(deck.cards[0])
-    expect(cards[2]).to.deep.equal(deck.cards[2])
-  })
-
-})
+  it("Should be able to create Cards", () => {
+    expect(game.createCards().slice(0, 3)).to.deep.equal(deck.cards);
+  });
+});
