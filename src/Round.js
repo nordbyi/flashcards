f0bca89bab5ee7f4d5e244ae22291354f26c416b
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-29 17:40:21 -0700

    Add returnCurrentCard() method

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,18 @@
+const Turn = require("./Turn")
+
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.currentCard = this.deck.cards[0];
+    this.turns = 0
+  }
+
+  returnCurrentCard() {
+    return this.currentCard
+  }
+
+  takeTurn() {
+    this.turns++
   }
 }
 
