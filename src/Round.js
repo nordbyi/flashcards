2d54fa64efd6de1a9d03ade7907067cd5a1023b8
Ian Nordby <ian.c.nordby@gmail.com>
2022-12-01 15:35:32 -0700

    Add Timer functionality

--- src/Round.js
+++ src/Round.js
@@ -6,8 +6,8 @@ class Round {
     this.currentCard = this.deck.cards[0];
     this.turns = 0;
     this.incorrectGuesses = [];
-    this.startTime = Date.now()
-    this.endTime
+    this.startTime = Date.now();
+    this.endTime;
   }
 
   returnCurrentCard() {
@@ -16,26 +16,35 @@ class Round {
 
   takeTurn(guess) {
     this.turns++;
-    const turn = new Turn(guess, this.currentCard)
-    if(!turn.evaluateGuess()) {
-      this.incorrectGuesses.push(this.currentCard.id)
+    const turn = new Turn(guess, this.currentCard);
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentCard.id);
     }
-    this.currentCard = this.deck.cards[this.turns]
-    return turn.giveFeedback()
+    this.currentCard = this.deck.cards[this.turns];
+    return turn.giveFeedback();
   }
 
   calculatePercentageCorrect() {
-    return (((this.incorrectGuesses.length / this.turns - 1) * -1) * 100).toFixed() * 1
+    return (
+      ((this.incorrectGuesses.length / this.turns - 1) * -1 * 100).toFixed() * 1
+    );
   }
 
   endRound() {
-    this.endTime = Date.now()
-    const roundMessage = `** Round over! ** You answered ${this.calculatePercentageCorrect()}% of the questions correctly!`
-    console.log(roundMessage)
-    console.log(`You completed the game in ${this.endTime - this.startTime} milliseconds`)
-    return roundMessage
+    this.endTime = Date.now();
+    const roundMessage = `** Round over! ** You answered ${this.calculatePercentageCorrect()}% of the questions correctly!`;
+    console.log(roundMessage);
+    console.log(this.finalTime());
+    return roundMessage;
   }
 
+  finalTime() {
+    const seconds = (this.endTime - this.startTime) / 1000;
+    const minutes = seconds / 60;
+    return `You completed the round in ${Math.floor(minutes)} minutes and ${(
+      seconds % 60
+    ).toFixed(3)} seconds!`;
+  }
 }
 
 module.exports = Round;
